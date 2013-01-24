75187457af007629e37374c981aae33db710b799
stevo <stevo@6b84dccb-955e-4d5a-98e1-60b1d0952c6b>
2013-01-24 09:13:30 +0000

    dodata classa kao param u plugin

--- www/public/scripts/t.tooltips.js
+++ www/public/scripts/t.tooltips.js
@@ -1,11 +1,7 @@
-/* 
-	Tooltips Script
-	Tirien.com
-	$Rev$
-
+/*  
 	tooltipsData = [ 
 		{ x:10, y:50, content:"text" }, 
-		{ x:20, y:50, content:"<b>html bold</b>" }, 
+		{ x:20, y:50, content:"<b>html bold</b>", className:"red" }, 
 		{ x:20, y:50, content:"<i>html italic</i>" } 
 	]
 	
@@ -17,7 +13,8 @@ function drawTooltips( wrapSelector, data ){
 	$(wrapSelector).css("position", "relative");
 	
 	for (i = 0; i < data.length; i++) {
-		$(wrapSelector).append('<div class="tooltipPoint" style="left:' + data[i].x + '%; top:' + data[i].y + '%;" ><div style="position:relative;z-index:100;"><div class="tooltipBox">' + data[i].content + '</div></div></div>')
+		className = typeof(data[i].className)=="undefined" ? "" : data[i].className;
+		$(wrapSelector).append('<div class="tooltipPoint ' + className + '" style="left:' + data[i].x + '%; top:' + data[i].y + '%;" ><div style="position:relative;z-index:100;"><div class="tooltipBox">' + data[i].content + '</div></div></div>')
 	}
 	
 	if( $(wrapSelector).find("img").length > 0 ){
