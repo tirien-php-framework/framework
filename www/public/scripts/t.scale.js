8ab7963dee1e9b742580cf65a7f9e5b65f820cbd
mladen <mladen@6b84dccb-955e-4d5a-98e1-60b1d0952c6b>
2013-05-29 13:01:53 +0000

    scale za sve elemente

--- www/public/scripts/t.scale.js
+++ www/public/scripts/t.scale.js
@@ -1,89 +1,91 @@
 ﻿/* 
-*	Scale Images jQuery Plugin
-*	Tirien.com
-*	$Rev$
+*   Scale Images jQuery Plugin
+*   Tirien.com
+*   $Rev$
 */
 
 $.fn.scale = function(ops){
+
     function Scale(el){
-        var parent;
+        var parent, elementWidth, elementHeight, elementRatio;
+
         if (typeof settings.parent !== 'undefined'){
             parent = settings.parent;
         }
         else{
             parent = el.parent();
         }
+
         var parentWidth = parent.width();
         var parentHeight = parent.height();
-        var tmpImg = new Image();
-        tmpImg.onload = function(){
-            var elementWidth = tmpImg.width;
-            var elementHeight = tmpImg.height;
 
-            if (settings.type == 'center'){
+        if( el.data("original-width")!=undefined && el.data("original-height")!=undefined ){
+            elementWidth = el.data("original-width");
+            elementHeight = el.data("original-height");
+        }
+        else{
+            elementWidth = el.width();
+            elementHeight = el.height();   
+            el.data("original-width", elementWidth);
+            el.data("original-height", elementHeight);
+        }
 
-                el.css({
-                    position: 'absolute',
-                    width: 'auto',
-                    height: 'auto',
-                    left: (parentWidth - elementWidth) /2,
-                    top: (parentHeight - elementHeight)/2
-                });
+        elementRatio = elementWidth/elementHeight;
 
-            }
-            else{
+        var b = (parentWidth / parentHeight) > (elementWidth / elementHeight);
+        var f = settings.type == 'fit';
 
-                var b = (parentWidth / parentHeight) > (elementWidth / elementHeight);
-                var f = settings.type == 'fit';
-                if ( (b && !f) || (!b && f) )
-                {
-                    el.css({
-                        width : '100%',
-                        height : 'auto'
-                    });
-                }
-                else
-                {
-                    el.css({
-                        width : 'auto',
-                        height : '100%'
-                    });
-                }
+        if ( (b && !f) || (!b && f) )
+        {
+            el.css({
+                width : '100%',
+                height : parentWidth / elementRatio
+            });
+        }
+        else
+        {
+            el.css({
+                width : parentHeight * elementRatio,
+                height : '100%'
+            });
+        }
 
-                if (settings.center){
-                    el.css({
-                        position:'absolute',
-                        left: '50%',
-                        marginLeft: -el.width() / 2,
-                        top: '50%',
-                        marginTop: -el.height() / 2,
-                    });
-                }		
+        if (settings.center){
+            el.css({
+                position:'absolute',
+                left: '50%',
+                marginLeft: -el.width() / 2,
+                top: '50%',
+                marginTop: -el.height() / 2,
+            });
+        }       
 
-                if(f){
-                    el.removeClass("scale").addClass('scale-f');
-                }
-                else{
-                    el.removeClass("scale-f").addClass('scale');
-                }
+        if(f){
+            el.removeClass("scale").addClass('scale-f');
+        }
+        else{
+            el.removeClass("scale-f").addClass('scale');
+        }
 
-            }
-        };
-        tmpImg.src = el.prop('src');
-        return;
+        return true;
     }
+
     var settings = $.extend( {
         type: 'fill',
-		attachEvents: 'true',
+        attachEvents: 'true',
         center: true
     }, ops);
+
     if (!this.length){
         return this;
     }
+
     this.each(function(ind,el){
         Scale($(el));
     });
+
     return this;
+
 };
 
 $(window).load(function(){
