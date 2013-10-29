4b825a876bfaa98f0cb0c4ca6b35af29ed56c9bc
mladen <mladen@6b84dccb-955e-4d5a-98e1-60b1d0952c6b>
2013-10-29 15:46:41 +0000

    ispravljen bug u detekciji mobile uredjaja

--- www/system/core.php
+++ www/system/core.php
@@ -42,7 +42,7 @@
 				$this->ipad = true;
 			}
 
-			if( preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]) ) {
+			if( preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|iphone|itouch|ipod|symbian|htc_|htc-|palmos|opera mini|iemobile|windows ce|nokia|fennec|kindle|mot |mot-|samsung|sonyericsson|^sie-|nintendo|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]) ) {
 				$this->mobile = true;
 			}
 
