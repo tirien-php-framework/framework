181ce95d0e3da3592dc6a60f1d6ff2fafc58c598
mladen <mladen@6b84dccb-955e-4d5a-98e1-60b1d0952c6b>
2014-02-09 21:24:52 +0000

    multi upload

--- www/application/library/File.php
+++ www/application/library/File.php
@@ -1,46 +1,88 @@
 <?php
-// TODO:ako ima vise fajlova mora da se hendla key 
+
 class File {
 
     static function getExtension( $str ) {
         return end(explode('.',$str));
     }
 
-    public static function upload($fileName, $uploadFolder = null) {
+    public static function upload( $fileName, $uploadFolder = null, $uploadFileName = null ) {
         if (isset($_FILES[$fileName])) {
-            global $config;
-            if ( empty($uploadFolder) ) {
-                $uploadFolder = 'public'.DIRECTORY_SEPARATOR.'uploads'.DIRECTORY_SEPARATOR;
-            }
 
-            $targetFolder = Path::appRoot( trim($uploadFolder, '/') );
+            if ( is_array($_FILES[$fileName]["name"]) ) {
+                
+                $return = array();
 
-            if (!is_dir($targetFolder)) {
-                if (!mkdir($targetFolder)) {
-                    return false;
-                }
-            }
+                foreach ($_FILES[$fileName]["name"] as $key => $value) {
 
-            if ($_FILES[$fileName]["error"] > 0) {
-                return false;
-            } 
-            else {
-                $targetFile = $targetFolder . DIRECTORY_SEPARATOR . self::sanitizeFilename($_FILES[$fileName]["name"]);
+                    $file = array(
+                        "name" => $_FILES[$fileName]["name"][$key],
+                        "type" => $_FILES[$fileName]["type"][$key],
+                        "tmp_name" => $_FILES[$fileName]["tmp_name"][$key],
+                        "error" => $_FILES[$fileName]["error"][$key],
+                        "size" => $_FILES[$fileName]["size"][$key]
+                        );
 
-                $i = 1;
-                while (file_exists($targetFile)) {
-                    $fileParts = pathinfo($targetFile);
-                    $targetFileName = $fileParts['filename'] . "_$i." . $fileParts['extension'];
-                    $targetFile = $fileParts['dirname'] . DIRECTORY_SEPARATOR . $targetFileName;
-                    $i++;
+                    $return[$key] = self::uploadSingle($file, $uploadFolder, $uploadFileName);
+
                 }
 
-                move_uploaded_file($_FILES[$fileName]["tmp_name"], $targetFile);
-                return strstr( $targetFile, $uploadFolder) ;
+                return $return;
+
             }
+            else{
+                return self::uploadSingle($_FILES[$fileName], $uploadFolder, $uploadFileName);
+            }
+
         } else {
             return false;
         }
+    }
+
+    public static function uploadSingle( $file, $uploadFolder = null, $uploadFileName = null )
+    {
+
+        if ( empty($uploadFolder) ) {
+            $uploadFolder = 'public'.DIRECTORY_SEPARATOR.'uploads'.DIRECTORY_SEPARATOR;
+        }
+
+        $targetFolder = Path::appRoot( trim($uploadFolder, '/') );
+
+        if (!is_dir($targetFolder)) {
+            if (!mkdir($targetFolder)) {
+                return false;
+            }
+        }
+
+        if ($file["error"] > 0) {
+            return false;
+        } 
+        else {
+
+            if ( empty($uploadFileName) ) {
+                $uploadFileName = $file["name"];
+            }
+            else if(strpos($uploadFileName, ".")===false){
+                $tmpFileParts = pathinfo($file["name"]);
+                $uploadFileName = $uploadFileName.".".$tmpFileParts['extension'];
+            }
+
+            $uploadFileName = self::sanitizeFilename($uploadFileName);
+            $targetFile = $targetFolder . DIRECTORY_SEPARATOR . $uploadFileName;
+
+            $i = 1;
+            while (file_exists($targetFile)) {
+                $fileParts = pathinfo($targetFile);
+                $uploadFileName = rtrim($fileParts['filename'], "_".($i-1)) . "_$i." . $fileParts['extension'];
+                $targetFile = $fileParts['dirname'] . DIRECTORY_SEPARATOR . $uploadFileName;
+                $i++;
+            }
+
+            move_uploaded_file($file["tmp_name"], $targetFile);
+            return $uploadFolder. DIRECTORY_SEPARATOR . $uploadFileName;
+            
+        }
+
     }
 
     public static function sanitizeFilename($filename) {
