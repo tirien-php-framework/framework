bcff7aa92c1dfdf610a7ccf04cb3ffed1120a24a
mladen <mladen@6b84dccb-955e-4d5a-98e1-60b1d0952c6b>
2014-04-07 09:24:34 +0000

    IE8 support za JS empty()

--- www/public/scripts/global.js
+++ www/public/scripts/global.js
@@ -1,3 +1,11 @@
+Object.size = function(obj) {
+    var size = 0, key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+};
+
 function empty(v){
-	return !v || typeof v == 'undefined' || ( typeof v == 'string' && v.replace(/[0| ]+/gm,'')=='' )	|| ( typeof v == 'object' && !Object.keys(v).length );
+	return !v || typeof v == 'undefined' || ( typeof v == 'string' && v.replace(/[0| ]+/gm,'')=='' )	|| ( typeof v == 'object' && !Object.size(v) );
 }
