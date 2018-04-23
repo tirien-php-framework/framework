a00e06c1fa0d8b8f721e765f6c00a3ec2cd758b9
mladen <mladen@6b84dccb-955e-4d5a-98e1-60b1d0952c6b>
2018-04-23 14:44:12 +0000

    default headers updated

--- www/application/layouts/default.php
+++ www/application/layouts/default.php
@@ -8,14 +8,16 @@
 	<meta name="description" content="<?php echo @$this->view->head['description'] ?>">
 	<meta name="keywords" content="<?php echo @$this->view->head['keywords'] ?>">
 
-	<meta property="og:title" content="<?php echo empty($this->view->head['og_title']) ? @$this->view->head['title'] : $this->view->head['og_title'] ?>"/>
-	<meta property="og:description" content="<?php echo empty($this->view->head['og_description']) ? @$this->view->head['description'] : $this->view->head['og_description'] ?>"/>
-	<meta property="og:image" content="<?php echo empty($this->view->head['og_image']) ? $this->view->head['image'] : $this->view->head['og_image'] ?>"/>
-	<meta property="og:type" content="website" /> 
-	<meta property="og:url" content="<?php echo Path::pageUrl() ?>" /> 
+	<meta property="og:title" content="<?php echo empty($this->view->head['og_title']) ? @$this->view->head['title'] : $this->view->head['og_title'] ?>">
+	<meta property="og:description" content="<?php echo empty($this->view->head['og_description']) ? @$this->view->head['description'] : $this->view->head['og_description'] ?>">
+	<meta property="og:image" content="<?php echo empty($this->view->head['og_image']) ? $this->view->head['image'] : $this->view->head['og_image'] ?>">
+	<meta property="og:type" content="website"> 
+	<meta property="og:url" content="<?php echo Path::pageUrl() ?>"> 
 
 	<meta name="format-detection" content="telephone=no">
-	<meta name="SKYPE_TOOLBAR" content="SKYPE_TOOLBAR_PARSER_COMPATIBLE" />
+	<meta name="SKYPE_TOOLBAR" content="SKYPE_TOOLBAR_PARSER_COMPATIBLE">
+	<meta name="robots" content="index, follow">
+	<link rel="canonical" href="<?php echo Path::urlBase(); ?>/">
 	
 	<link rel="stylesheet" href="css/reset.css">
 	<link rel="stylesheet" href="css/global.min.css">
