9df6a0f2c03b00c647a9017871a140adac6a58aa
Mladen Janjetovic <4288643+MladenJanjetovic@users.noreply.github.com>
2018-11-05 14:37:16 +0100

    use .sqlite extension fix, crlf to lf

--- application/library/Image.php
+++ application/library/Image.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 
 if (is_file(__DIR__ . '/../../vendor/autoload.php') && is_readable(__DIR__ . '/../../vendor/autoload.php')) {
     require_once __DIR__ . '/../../vendor/autoload.php';
