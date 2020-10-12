daf773dc515a0715ecd66fd892e3a6483375e8cc
Mladen Janjetovic <4288643+MladenJanjetovic@users.noreply.github.com>
2020-10-12 11:18:23 +0200

    User Model docs

--- application/models/User.php
+++ application/models/User.php
@@ -1,18 +1,36 @@
 <?php 
 
-require(dirname(__FILE__).'/'.'Query_Builder.php'); 
+require(dirname(__FILE__).'/'.'Query_Builder.php');
+
+/**
+ * Class Model_User
+ */
 
 Class Model_User extends Query_Builder
 {
+    /**
+     * @var string
+     */
+    public $tablePrefix = "";
 
-	public $tablePrefix = "";
-//todo:srediti docs za ovaj model
-	public function getUser( $data )
+    /**
+     * Get an active User from database
+     *
+     * @param $data
+     * @return mixed
+     */
+    public function getUser($data )
 	{
 		return DB::query( "SELECT * FROM user WHERE username=:username AND password_hash=:password_hash AND status=1", $data, true );		
 	}
-	
-	public function createUser($data)
+
+    /**
+     * Create User in database
+     *
+     * @param $data
+     * @return false
+     */
+    public function createUser($data)
 	{
 		if( !empty($data['username']) && !empty($data['password']) ) {
 			$user_exists = DB::query( "SELECT * FROM user WHERE username=?", $data['username'] );
@@ -30,7 +48,13 @@ Class Model_User extends Query_Builder
 		}
 	}
 
-	public function changePassword($data){
+    /**
+     * Update User password in database
+     *
+     * @param $data
+     * @return bool
+     */
+    public function changePassword($data){
 
 		if( !empty($data['username']) && !empty($data['password']) ) {
 			
